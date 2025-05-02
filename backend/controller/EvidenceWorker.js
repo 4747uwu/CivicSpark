e81f107013c50e06c36d869799ab1995af0eccc8
Abhishek Shukla <4747uwu@gmail.com>
2025-05-02 16:28:59 +0530

    EvidenceWorker

--- backend/controller/EvidenceWorker.js
+++ backend/controller/EvidenceWorker.js
@@ -3,7 +3,7 @@ import { validateEvidence } from '../services/geminiService';
 import CampaignEvidence from '../models/CampaignEvidence';
 
 const worker = new Worker('evidenceValidation', async (job) => {
-  const { evidenceId } = job;
+  const { evidenceId } = job.data;
   
   try {
     const evidence = await CampaignEvidence.findById(evidenceId);
